39d584c356fa61ed7720d8213c4e9c0372d354e0
maisparavyan-cyber <mais.paravyan@gmail.com>
2025-12-25 00:32:39 +0400

    Delete Footer.js

--- Footer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { IoLogoInstagram } from "react-icons/io";
-
-export default function Footer() {
-  return (
-    <footer>   <IoLogoInstagram />maisparavyan </footer>
-  )
-}
